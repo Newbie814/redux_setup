8528017fb94ac373a201376cd42fa2a13b9e1515
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-21 00:00:56 -0400

    setup cart slice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,17 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  name: [],
+  amount: 0,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+});
+
+console.log(cartSlice);
+
+export default cartSlice.reducer;
